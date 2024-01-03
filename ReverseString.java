26ee83f68e03d4d99e35ef84cb4efb531acea979
Sabuj <www.sabujgolui@gmail.com>
2024-01-03 09:18:49 +0530

    Reverse the String

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public String solve(String A) {
+        
+        String[] w = A.split("\\s+");
+        StringBuilder rw = new StringBuilder();
+        for (int i = w.length - 1; i >= 0; i--) {
+            rw.append(w[i]).append(" ");
+        }
+        return rw.toString().trim();
+    }
+}
